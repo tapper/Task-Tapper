c4216b1d649417bc1237be52ae514922d0cb9bf1
Steffen Schwigon <steffen.schwigon@amd.com>
2012-09-06 13:40:52 +0200

    initial Tapper bundle

---
+++ lib/Task/Tapper.pm
@@ -0,0 +1,14 @@
+use strict;
+use warnings;
+package Task::Tapper;
+# ABSTRACT: Tapper - max dependencies for hacking on Tapper
+
+=pkg Task::Tapper::Installer
+
+=pkg Task::Tapper::Client
+
+=pkg Task::Tapper::Server
+
+=pkg Task::Tapper::Server::Automation
+
+1;
